ceb41f759f5be2c6646a425c9858ce6c57e01fc8
Dheerajkumar36 <146161774+Dheerajkumar36@users.noreply.github.com>
2025-11-08 23:00:46 +0530

    Add files via upload

---
+++ Weather-Based Activity Suggestion App/src/main/java/com/example/weather/WeatherActivityApplication.java
@@ -0,0 +1,11 @@
+package com.example.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherActivityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherActivityApplication.class, args);
+    }
+}
